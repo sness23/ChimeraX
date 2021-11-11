77862b424a6b07315fcd643bc3ef2bff9eba2ca6
sness <snessnet@gmail.com>
2021-11-11 06:08:25 -0800

    First stab at dark mode

--- src/bundles/ui/src/gui.py
+++ src/bundles/ui/src/gui.py
@@ -160,6 +160,23 @@ class UI(QApplication):
         from chimerax import app_dirs as ad
         QApplication.__init__(self, [ad.appname])
 
+        # sness - dark mode
+        # palette = QPalette()
+        # palette.setColor(QPalette.Window, QColor(53, 53, 53))
+        # palette.setColor(QPalette.WindowText, Qt.white)
+        # palette.setColor(QPalette.Base, QColor(25, 25, 25))
+        # palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
+        # palette.setColor(QPalette.ToolTipBase, Qt.black)
+        # palette.setColor(QPalette.ToolTipText, Qt.white)
+        # palette.setColor(QPalette.Text, Qt.white)
+        # palette.setColor(QPalette.Button, QColor(53, 53, 53))
+        # palette.setColor(QPalette.ButtonText, Qt.white)
+        # palette.setColor(QPalette.BrightText, Qt.red)
+        # palette.setColor(QPalette.Link, QColor(42, 130, 218))
+        # palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
+        # palette.setColor(QPalette.HighlightedText, Qt.black)
+        # app.setPalette(palette)
+
         from Qt import using_qt5
         if using_qt5:
             # Improve toolbar icon quality on retina displays
